765f41f3eacc2aad78eef13e468bda30f92b8c0e
FeV Chen <413150803@qq.com>
2024-03-25 22:52:01 +0800

    2024/03/25 1. 常规性更新，注意，GBR的网格搜索存在报错！

--- dataset of no loss/model/GBR-grid search.py
+++ dataset of no loss/model/GBR-grid search.py
@@ -5,11 +5,14 @@ from sklearn.metrics import r2_score
 from sklearn.model_selection import KFold, GridSearchCV
 
 # Load the cleaned dataset
-df = pd.read_csv('carbon_20240320.csv')  # Update this path to your dataset
+df = pd.read_csv('carbon_20240322.csv')  # Update this path to your dataset
+
+# One-hot encode the categorical columns 'Electrolyte' and 'Current collector'
+df_encoded = pd.get_dummies(df, columns=['Electrolyte', 'Current collector'])
 
 # Features and Target separation
-X = df.iloc[:, :-1]  # Exclude the last column (target)
-y = df.iloc[:, -1]  # Target column
+X = df_encoded.drop('Cs', axis=1)
+y = df_encoded['Cs']
 
 # Initialize K-Fold Cross-Validator
 kf = KFold(n_splits=10, shuffle=True, random_state=21)
@@ -17,21 +20,32 @@ kf = KFold(n_splits=10, shuffle=True, random_state=21)
 # Set up the parameter grid
 param_grid = {
     'n_estimators': range(100, 2100, 100),
-    'learning_rate': np.arange(0.05, 0.325, 0.025),
+    'learning_rate': np.arange(0, 0.325, 0.025),
     'max_depth': range(2, 7)
 }
 
 # Initialize the model with Gradient Boosting Regression
 gbr = GradientBoostingRegressor(random_state=21)
 
-grid_search = GridSearchCV(estimator=gbr, param_grid=param_grid, cv=kf, scoring='r2', verbose=2, n_jobs=-1)
+scoring = {
+    'MAE': 'neg_mean_absolute_error',
+    'RMSE': 'neg_root_mean_squared_error',
+}
+
+grid_search = GridSearchCV(estimator=gbr,
+                           param_grid=param_grid,
+                           cv=kf,
+                           scoring=scoring,
+                           verbose=2,
+                           refit=False,
+                           n_jobs=-1)
 
 # Perform Grid Search
 grid_search.fit(X, y)
 
 # Best parameters and score
 print("Best parameters:", grid_search.best_params_)
-print("Best score (R2):", grid_search.best_score_)
+print("Best score:", grid_search.best_score_)
 
 # Retrieve the best model
 best_model = grid_search.best_estimator_
--- dataset of no loss/model/GBR.py
+++ dataset of no loss/model/GBR.py
@@ -1,11 +1,11 @@
-import numpy as np
 import pandas as pd
 from sklearn.ensemble import GradientBoostingRegressor
-from sklearn.metrics import mean_squared_error, mean_absolute_error, mean_absolute_percentage_error, r2_score
-from sklearn.model_selection import KFold
+from sklearn.metrics import mean_absolute_error, mean_absolute_percentage_error, r2_score, \
+    root_mean_squared_error
+from sklearn.model_selection import train_test_split, KFold
 
 # Load the cleaned dataset
-df = pd.read_csv('carbon_20240320.csv')
+df = pd.read_csv('carbon_20240322.csv')
 
 # One-hot encode the categorical columns 'Electrolyte' and 'Current collector'
 df_encoded = pd.get_dummies(df, columns=['Electrolyte', 'Current collector'])
@@ -13,15 +13,39 @@ df_encoded = pd.get_dummies(df, columns=['Electrolyte', 'Current collector'])
 # Features and Target separation
 X = df_encoded.drop('Cs', axis=1)
 y = df_encoded['Cs']
-pd.set_option('display.max_columns', None)
-print(X)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=21)
+
+# Initialize the model with Gradient Boosting Regression
+gbr = GradientBoostingRegressor(n_estimators=2000,
+                                learning_rate=0.15,
+                                max_depth=4,
+                                min_samples_leaf=1,
+                                min_samples_split=2,
+                                random_state=21)
+gbr.fit(X_train, y_train)
+y_pred_train = gbr.predict(X_train)
+y_pred_test = gbr.predict(X_test)
+
+r2_train = r2_score(y_train, y_pred_train)
+r2_test = r2_score(y_test, y_pred_test)
+mae_train = mean_absolute_error(y_train, y_pred_train)
+mae_test = mean_absolute_error(y_test, y_pred_test)
+mape_train = mean_absolute_percentage_error(y_train, y_pred_train)
+mape_test = mean_absolute_percentage_error(y_test, y_pred_test)
+rmse_train = root_mean_squared_error(y_train, y_pred_train)
+rmse_test = root_mean_squared_error(y_test, y_pred_test)
+
+results = pd.DataFrame({
+    'Metric': ['R2', 'MAE', 'MAPE', 'RMSE'],
+    'train set': [r2_train, mae_train, mape_train, rmse_train],
+    'Test set': [r2_test, mae_test, mape_test, rmse_test]
+})
+
+print(results)
 
 # Initialize 10-Fold Cross Validator
 kf = KFold(n_splits=10, shuffle=True, random_state=21)
-
-# Initialize the model with Gradient Boosting Regression
-gbr = GradientBoostingRegressor(n_estimators=2000, learning_rate=0.125, max_depth=3,
-                                min_samples_leaf=1, min_samples_split=2, random_state=21)
 
 # Prepare DataFrame to store metrics for each fold
 metrics_df = pd.DataFrame(columns=['Fold', 'R2', 'MAE', 'MAPE', 'RMSE'])
@@ -44,7 +68,7 @@ for fold, (train_index, test_index) in enumerate(kf.split(X), start=1):
         'R2': r2_score(y_test, y_pred),
         'MAE': mean_absolute_error(y_test, y_pred),
         'MAPE': mean_absolute_percentage_error(y_test, y_pred),
-        'RMSE': np.sqrt(mean_squared_error(y_test, y_pred))
+        'RMSE': root_mean_squared_error(y_test, y_pred),
     })
 
 # Convert list of rows to DataFrame
