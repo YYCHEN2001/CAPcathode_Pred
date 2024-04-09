f7d4295167cdce240c2c725cd48bf210f3284159
FeV Chen <413150803@qq.com>
2024-04-09 09:52:42 +0800

    2024/04/09 feat(MLP): MLP
    1. 新增了MLP算法

---
+++ model/MLP/MLP.py
@@ -0,0 +1,27 @@
+from sklearn.neural_network import MLPRegressor
+
+from dataset_function import dataset_load, dataset_split
+from model_evaluation import train_evaluate, plot_actual_vs_predicted
+
+# Split the dataset into training and testing sets, using quantile-based stratification for the target variable.
+df = dataset_load('../../dataset/carbon_202404_v2.csv')
+X_train, X_test, y_train, y_test = dataset_split(df, test_size=0.3, random_state=21, target='Cs')
+
+# Initialize the model with MLP Regression
+mlp = MLPRegressor(hidden_layer_sizes=(50, 50, 80),
+                   activation='relu',
+                   solver='adam',
+                   alpha=0.01,
+                   batch_size='auto',
+                   learning_rate='constant',
+                   learning_rate_init=0.01,
+                   max_iter=200,
+                   shuffle=True,
+                   random_state=21)
+
+# Train and evaluate the model
+results = train_evaluate(mlp, X_train, y_train, X_test, y_test)
+print(results)
+
+# Plot the actual vs predicted values
+plot_actual_vs_predicted(y_train, mlp.predict(X_train), y_test, mlp.predict(X_test))
--- model/lightGBM/lGBM.py
+++ model/lightGBM/lGBM.py
@@ -9,12 +9,16 @@ df = dataset_load('../../dataset/carbon_202404_v2.csv')
 X_train, X_test, y_train, y_test = dataset_split(df, test_size=0.3, random_state=21, target='Cs')
 
 # Initialize the model with LightGBM Regression
-lgbm = LGBMRegressor(n_estimators=300,
-                     max_depth=-1,
-                     min_child_samples=2,
+lgbm = LGBMRegressor(n_estimators=1000,
+                     max_depth=8,
+                     min_child_samples=3,
+                     min_child_weight=1.8,
                      num_leaves=5,
-                     subsample=0.5,
-                     learning_rate=0.15,
+                     subsample=0.7,
+                     learning_rate=0.16,
+                     colsample_bytree=0.97,
+                     colsample_bylevel=0.75,
+                     colsample_bynode=0.69,
                      random_state=21)
 # Train and evaluate the model
 results = train_evaluate(lgbm, X_train, y_train, X_test, y_test)
